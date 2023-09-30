be576b2bf60dc862a702b3e9467b7c58ba422696
DESKTOP-LRU92KK\pc <caugioi.lr@gmail.com>
2023-09-30 16:07:58 +0700

    Add project files.

---
+++ 1.2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Tạo project C# thực hiện việc in ra màn hình thông tin giới thiệu về bản thân, biên dịch,
+//chạy và kiểm tra kết quả
+
+Console.WriteLine("Ho va ten: THUAN THIEN");
+Console.WriteLine("Tuoi: 18 lan thu n");
+Console.WriteLine("Dia chi: Nam Tu Liem");
+Console.WriteLine("So dien thoai: 0353xxxxxx");
+Console.WriteLine("Dang hoc Csharpppppp");
+
+
+
